47993990573a049038c9b72fe0dd1fbd7c4cfc22
Yaqub Mahmoud <yaqub320@gmail.com>
2019-03-30 15:11:07 +0900

    Restricted Player Movement on Screen

--- Dodge-The-Blocks-Game/Assets/Scripts/Player.cs
+++ Dodge-The-Blocks-Game/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 15f;
+    public float mapWidth = 5f;
 
     private Rigidbody2D rb;
 
@@ -22,7 +23,11 @@ public class Player : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
 
-            rb.MovePosition(rb.position + Vector2.right * x);
+        Vector2 newPosition = rb.position + Vector2.right * x;
 
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+
+        rb.MovePosition(newPosition);
     }
 }
