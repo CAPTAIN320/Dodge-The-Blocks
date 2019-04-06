2430b4f9adaff74af893d4b28214b3be41b5a52e
Yaqub Mahmoud <yaqub320@gmail.com>
2019-04-06 22:53:07 +0900

    Bug Fix

--- Dodge-The-Blocks-Game/Assets/Scripts/Player.cs
+++ Dodge-The-Blocks-Game/Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float speed = 10f;
-    public float mapWidth = 3f;
+    public float speed = 15f;
+    public float mapWidth = 5f;
 
     private Rigidbody2D rb;
 
@@ -25,7 +25,7 @@ public class Player : MonoBehaviour
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
 
         // Move player in both left & right by multiplying by x
-        Vector2 newPosition = rb.position + (Vector2.right * x); // 
+        Vector2 newPosition = rb.position + Vector2.right * x; // 
 
         // Restrict player position on screen
         newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
